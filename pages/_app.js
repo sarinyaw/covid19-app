2783f276cfd4dd98abf77c5eeebc3ee2239d8d00
Sarinya Wongmueng <51124721+sarinyaw@users.noreply.github.com>
2021-01-30 18:35:10 +0700

    remove import react-datepicker css

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import 'react-date-picker/dist/DatePicker.css'
 import 'react-calendar/dist/Calendar.css'
 import 'react-time-picker/dist/TimePicker.css'
 import 'react-clock/dist/Clock.css'
-import 'react-datepicker/dist/react-datepicker.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
